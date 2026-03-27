99da8281c1b41a21f51d95731c6b7a27d950ad89
Krrish Mangalani <krrishmangalani5@gmail.com>
2026-03-27 18:58:05 +0530

    Updated SkillGapController.cs

--- Models/SkillGapModel.cs
+++ Models/SkillGapModel.cs
@@ -1,16 +1,21 @@
-﻿namespace API.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace API.Models;
 
 public class SkillGapModel
 {
+    [JsonPropertyName("userId")]
     public string UserId { get; set; }
+
+    [JsonPropertyName("skillId")]
     public string SkillId { get; set; }
 
-    public int BeginnerPerfect { get; set; }
-    public int MidPerfect { get; set; }
-    public int ExpertPerfect { get; set; }
-
+    [JsonPropertyName("completionPercentage")]
     public double CompletionPercentage { get; set; }
+
+    [JsonPropertyName("skillGapPercentage")]
     public double SkillGapPercentage { get; set; }
 
+    [JsonPropertyName("isMastered")]
     public bool IsMastered { get; set; }
 }
